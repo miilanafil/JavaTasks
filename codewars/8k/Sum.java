bb610c40b29f78f321e44932be0a60915c01b436
milanafil <125284843+miilanafil@users.noreply.github.com>
2026-08-22 14:25:06 +0300

    change

--- codewars/8k/Sum.java
+++ codewars/8k/Sum.java
@@ -1,18 +1,24 @@
 
-public class Sum {
-    public static int expressionsMatter(int a, int b, int c) {
-        int r1 = a + b + c;
-        int r2 = a * b * c;
-        int r3 = a + b * c;
-        int r4 = (a + b) * c;
-        int r5 = a * (b + c);
 
-        int max = r1;
-        if (r2 > max) max = r2;
-        if (r3 > max) max = r3;
-        if (r4 > max) max = r4;
-        if (r5 > max) max = r5;
+public static int exepta(int a, int b, int c) {
+    int r1 = a + b + c;
+    int r2 = a * b * c;
+    int r3 = a + b * c;
+    int r4 = (a + b) * c;
+    int r5 = a * (b + c);
 
-        return max;
-    }
+    int max = r1;
+    if (r2 > max) max = r2;
+    if (r3 > max) max = r3;
+    if (r4 > max) max = r4;
+    if (r5 > max) max = r5;
+    System.out.println(max);
+    return max;
+}
+
+void main() {
+
+
+    exepta(6,4,5);
+
 }
